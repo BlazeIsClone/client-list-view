3253d30c60ae8bfb9731337a4778c26ccaca68af
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-06-30 22:13:07 +0530

    config: basic folder structure

---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import { Demo } from "../components";
+import "./App.css";
+
+export const App = (): JSX.Element => (
+  <div className="App">
+    <Demo />
+  </div>
+);
---
+++ src/components/demo/Demo.tsx
@@ -0,0 +1,7 @@
+import logo from "./assets/logo.svg";
+
+export const Demo = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+  </header>
+);
