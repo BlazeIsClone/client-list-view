23939b2c51d8d85c34ba80ab5fd5f5c429359a17
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-08-20 20:22:39 +0530

    refactor: partial interceptors for soc

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -1,5 +1,4 @@
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { endpoints } from './endpoints';
+import axios from 'axios';
 
 const { APP_API_ENTRYPOINT } = import.meta.env;
 
@@ -8,72 +7,3 @@ export const instance = axios.create({
 	headers: { 'Content-Type': 'application/json' },
 	withCredentials: true,
 });
-
-let refresh = false;
-
-const refreshToken = async (): Promise<AxiosResponse> => {
-	const response = await axios.post(
-		endpoints.refreshToken,
-		{},
-		{ withCredentials: true }
-	);
-
-	return response;
-};
-
-const onRequest = async (
-	request: AxiosRequestConfig
-): Promise<AxiosRequestConfig> => {
-	try {
-		const response = await refreshToken();
-
-		if (response.status === 200) {
-			request.headers = {
-				Authorization: `Bearer ${response.data.access_token}`,
-			};
-		}
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response?.status === 401) {
-			window.location.href = endpoints.login;
-		}
-	}
-
-	return request;
-};
-
-const onRequestError = (error: AxiosError): Promise<AxiosError> => {
-	return Promise.reject(error);
-};
-
-const onResponse = (response: AxiosResponse): AxiosResponse => {
-	return response;
-};
-
-const onResponseError = async (
-	error: AxiosError
-): Promise<AxiosError | AxiosResponse> => {
-	if (error.response?.status === 401 && !refresh) {
-		refresh = true;
-		try {
-			const response = await refreshToken();
-
-			if (response.status === 200) {
-				error.config.headers = {
-					Authorization: `Bearer ${response.data.access_token}`,
-				};
-
-				return axios(error.config);
-			}
-		} catch (error) {
-			if (axios.isAxiosError(error) && error.response?.status === 401) {
-				window.location.href = endpoints.login;
-			}
-		}
-	}
-
-	refresh = false;
-	return Promise.reject(error);
-};
-
-instance.interceptors.request.use(onRequest, onRequestError);
-instance.interceptors.response.use(onResponse, onResponseError);
---
+++ src/services/interceptors.ts
@@ -0,0 +1,73 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+import { endpoints } from './endpoints';
+import { instance } from './instance';
+
+let refresh = false;
+
+const refreshToken = async (): Promise<AxiosResponse> => {
+	const response = await axios.post(
+		endpoints.refreshToken,
+		{},
+		{ withCredentials: true }
+	);
+
+	return response;
+};
+
+const onRequest = async (
+	request: AxiosRequestConfig
+): Promise<AxiosRequestConfig> => {
+	try {
+		const response = await refreshToken();
+
+		if (response.status === 200) {
+			request.headers = {
+				Authorization: `Bearer ${response.data.access_token}`,
+			};
+		}
+	} catch (error) {
+		if (axios.isAxiosError(error) && error.response?.status === 401) {
+			window.location.href = endpoints.login;
+		}
+	}
+
+	return request;
+};
+
+const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+	return Promise.reject(error);
+};
+
+const onResponse = (response: AxiosResponse): AxiosResponse => {
+	return response;
+};
+
+const onResponseError = async (
+	error: AxiosError
+): Promise<AxiosError | AxiosResponse> => {
+	if (error.response?.status === 401 && !refresh) {
+		refresh = true;
+		try {
+			const response = await refreshToken();
+
+			if (response.status === 200) {
+				error.config.headers = {
+					Authorization: `Bearer ${response.data.access_token}`,
+				};
+
+				return axios(error.config);
+			}
+		} catch (error) {
+			if (axios.isAxiosError(error) && error.response?.status === 401) {
+				window.location.href = endpoints.login;
+			}
+		}
+	}
+
+	refresh = false;
+	return Promise.reject(error);
+};
+
+instance.interceptors.request.use(onRequest, onRequestError);
+instance.interceptors.response.use(onResponse, onResponseError);
